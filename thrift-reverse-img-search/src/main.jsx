502a66eaa42774b1da13db249dd6cd87c164e7b3
Evelyn Brannen <evbranne@asu.edu>
2026-09-05 23:51:43 -0700

    capitalization error

--- thrift-reverse-img-search/src/main.jsx
+++ thrift-reverse-img-search/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Gate from './gate.jsx'
+import Gate from './Gate.jsx'
 import App from './App.jsx'
 import './index.css'
 
