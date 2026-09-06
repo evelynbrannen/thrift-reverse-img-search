712f3200357ea96ff3195773508bf288d7e05d9f
Evelyn Brannen <evbranne@asu.edu>
2026-09-05 23:43:47 -0700

    setup github pages

--- thrift-reverse-img-search/vite.config.js
+++ thrift-reverse-img-search/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: { exclude: ['@huggingface/transformers'] },
+  base: '/thrift-reverse-img-search/',
 })
